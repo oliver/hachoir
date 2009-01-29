50f931cef83a9d591c054b86b625e593bc2c7225
Oliver Gerlich <oliver.gerlich@gmx.de>
2009-01-29 21:25:50 +0100

    display file size with filesizeHandler

--- hachoir-parser/hachoir_parser/archive/prs_pak.py
+++ hachoir-parser/hachoir_parser/archive/prs_pak.py
@@ -11,11 +11,12 @@ from hachoir_parser import Parser
 from hachoir_core.field import (ParserError,
     UInt8, UInt16, UInt32, String, SubFile, RawBytes, FieldSet)
 from hachoir_core.endian import LITTLE_ENDIAN, BIG_ENDIAN
+from hachoir_core.text_handler import filesizeHandler
 
 class FileEntry(FieldSet):
     def createFields(self):
         yield String(self, "filename", 56, truncate="\0")
-        yield UInt32(self, "size")
+        yield filesizeHandler(UInt32(self, "size"))
         yield SubFile(self, "data", self["size"].value, filename=self["filename"].value)
 
 
